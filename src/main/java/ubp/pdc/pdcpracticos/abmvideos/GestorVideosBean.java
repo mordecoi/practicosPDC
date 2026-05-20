5e338f5be461b19689820982cbe64ff51791e551
mordecoi <f.gomezgeneiro@yahoo.com>
2026-05-20 16:04:29 -0300

    modificando GestorVideosBean

--- src/main/java/ubp/pdc/pdcpracticos/abmvideos/GestorVideosBean.java
+++ src/main/java/ubp/pdc/pdcpracticos/abmvideos/GestorVideosBean.java
@@ -6,15 +6,17 @@ import java.util.LinkedList;
 public class GestorVideosBean {
 
     LinkedList<VideoBean> listaVideos;
-    HashMap<Integer, String> categorias = new HashMap<Integer, String>();
-    categorias.put(1, "BLUE");
-    categorias.put(2, "CLÁSICA");
-    categorias.put(3, "COUNTRY");
-    categorias.put(4, "FUNK");
-    categorias.put(5, "HIP HOP");
-    categorias.put(6, "JAZZ");
-    categorias.put(7, "POP");
-    categorias.put(8, "ROCK");
-    categorias.put(9, "REGGAETON y TRAP");
-    categorias.put(10, "ROCK ALTERNATIVO");
+//    HashMap<Integer, String> lcategorias = new HashMap<Integer, String>();
+//    lcategorias.put(1, "BLUE");
+//    categorias.put(2, "CLÁSICA");
+//    categorias.put(3, "COUNTRY");
+//    categorias.put(4, "FUNK");
+//    categorias.put(5, "HIP HOP");
+//    categorias.put(6, "JAZZ");
+//    categorias.put(7, "POP");
+//    categorias.put(8, "ROCK");
+//    categorias.put(9, "REGGAETON y TRAP");
+//    categorias.put(10, "ROCK ALTERNATIVO");
+
+
 }
